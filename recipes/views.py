f8487f781c56fb07bd48f77ef3a751a8d24d8a07
eriknathan <eriknathan.contato@gmail.com>
2024-12-25 11:23:22 -0300

    feat: create templates base

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Sobre mim!")
+    return render(request, 'recipes/home.html')
